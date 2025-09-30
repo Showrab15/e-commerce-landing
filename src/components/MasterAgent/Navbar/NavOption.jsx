858898c8eebd9ec16a6fa3eb679987251d1b217a
Showrab15 <supto50showrab@gmail.com>
2025-09-30 14:46:36 +0600

    navbar link fixed

--- src/components/MasterAgent/Navbar/NavOption.jsx
+++ src/components/MasterAgent/Navbar/NavOption.jsx
@@ -5,19 +5,19 @@ export default function NavOption() {
   const [dropdownVisible, setDropdownVisible] = useState(null); // Manage dropdown visibility for multiple dropdowns
 
   const tabs = [
-    { label: "হোম পেইজ", link: "/" }, // Added link to হোম পেইজ
+    { label: "হোম পেইজ", link: "/agent" }, // Added link to হোম পেইজ
     { label: " প্রশ্ন উত্তর" },
     { label: " সাইট" },
     { label: "এজেন্টকে খুজুন" },
     { label: "এজেন্ট লিস্ট" },
-    { label: "কাস্টমার সার্ভিস" }
+    { label: "কাস্টমার সার্ভিস" },
   ];
 
   const agentOptions = [
-    { label: "মাস্টার এজেন্ট লিস্ট", link: "/master-agent" },
-    { label: "সুপার এজেন্ট লিস্ট", link: "/master-agent" },
-    { label: "সাব এডমিন লিস্ট", link: "/master-agent" },
-    { label: "সাইট এডমিন লিস্ট" , link: "/master-agent"},
+    { label: "মাস্টার এজেন্ট লিস্ট", link: "/agent" },
+    { label: "সুপার এজেন্ট লিস্ট", link: "/agent" },
+    { label: "সাব এডমিন লিস্ট", link: "/agent" },
+    { label: "সাইট এডমিন লিস্ট", link: "/agent" },
   ];
 
   const generalQOptions = [
@@ -39,7 +39,9 @@ export default function NavOption() {
                 <a
                   href={tab.link}
                   className={`cursor-pointer px-8 py-2  font-semibold transition-colors duration-200 ${
-                    activeTab === tab.label ? "bg-black text-white" : "hover:border-b-2 border-white hover:text-white"
+                    activeTab === tab.label
+                      ? "bg-black text-white"
+                      : "hover:border-b-2 border-white hover:text-white"
                   }`}
                   onClick={() => setActiveTab(tab.label)}
                 >
@@ -49,12 +51,16 @@ export default function NavOption() {
                 <span
                   onClick={() => setActiveTab(tab.label)}
                   onMouseEnter={() => {
-                    if (tab.label === "এজেন্ট লিস্ট") setDropdownVisible("এজেন্ট লিস্ট");
-                    if (tab.label === "সাধারণ প্রশ্ন উত্তর") setDropdownVisible("সাধারণ প্রশ্ন উত্তর");
+                    if (tab.label === "এজেন্ট লিস্ট")
+                      setDropdownVisible("এজেন্ট লিস্ট");
+                    if (tab.label === "সাধারণ প্রশ্ন উত্তর")
+                      setDropdownVisible("সাধারণ প্রশ্ন উত্তর");
                   }}
                   onMouseLeave={() => {
-                    if (tab.label === "এজেন্ট লিস্ট") setDropdownVisible("এজেন্ট লিস্ট");
-                    if (tab.label === "সাধারণ প্রশ্ন উত্তর") setDropdownVisible("সাধারণ প্রশ্ন উত্তর");
+                    if (tab.label === "এজেন্ট লিস্ট")
+                      setDropdownVisible("এজেন্ট লিস্ট");
+                    if (tab.label === "সাধারণ প্রশ্ন উত্তর")
+                      setDropdownVisible("সাধারণ প্রশ্ন উত্তর");
                   }}
                   className={`px-8 py-2  font-semibold cursor-pointer transition-colors duration-200 ${
                     activeTab === tab.label
@@ -67,46 +73,50 @@ export default function NavOption() {
               )}
 
               {/* Dropdown for "এজেন্ট লিস্ট" */}
-              {tab.label === "এজেন্ট লিস্ট" && dropdownVisible === "এজেন্ট লিস্ট" && (
-                <div
-                  onMouseEnter={() => setDropdownVisible("এজেন্ট লিস্ট")}
-                  onMouseLeave={() => setDropdownVisible(null)}
-                  className="text-md w-48 absolute top-full left-0 mt-2 bg-[#2b2922] shadow-lg z-10"
-                >
-                  {agentOptions.map((option, idx) => (
-                    <a
-                      key={idx}
-                      href={option.link || "#"} // Only add href if a link exists
-                      className={`block px-4 py-2 text-black hover:text-red-400 ${
-                        option.link ? "cursor-pointer" : "cursor-default"
-                      }`}
-                    >
-                      {option.label}
-                    </a>
-                  ))}
-                </div>
-              )}
+              {tab.label === "এজেন্ট লিস্ট" &&
+                dropdownVisible === "এজেন্ট লিস্ট" && (
+                  <div
+                    onMouseEnter={() => setDropdownVisible("এজেন্ট লিস্ট")}
+                    onMouseLeave={() => setDropdownVisible(null)}
+                    className="text-md w-48 absolute top-full left-0 mt-2 bg-[#2b2922] shadow-lg z-10"
+                  >
+                    {agentOptions.map((option, idx) => (
+                      <a
+                        key={idx}
+                        href={option.link || "#"} // Only add href if a link exists
+                        className={`block px-4 py-2 text-black hover:text-red-400 ${
+                          option.link ? "cursor-pointer" : "cursor-default"
+                        }`}
+                      >
+                        {option.label}
+                      </a>
+                    ))}
+                  </div>
+                )}
 
               {/* Dropdown for "সাধারণ প্রশ্ন উত্তর" */}
-              {tab.label === "সাধারণ প্রশ্ন উত্তর" && dropdownVisible === "সাধারণ প্রশ্ন উত্তর" && (
-                <div
-                  onMouseEnter={() => setDropdownVisible("সাধারণ প্রশ্ন উত্তর")}
-                  onMouseLeave={() => setDropdownVisible(null)}
-                  className="text-md w-48 absolute top-full left-0 mt-2 bg-white shadow-lg z-10"
-                >
-                  {generalQOptions.map((option, idx) => (
-                    <a
-                      key={idx}
-                      href={option.link || "#"} // Only add href if a link exists
-                      className={`block px-4 py-2 text-black hover:border-b-2 border-white hover:text-white ${
-                        option.link ? "cursor-pointer" : "cursor-default"
-                      }`}
-                    >
-                      {option.label}
-                    </a>
-                  ))}
-                </div>
-              )}
+              {tab.label === "সাধারণ প্রশ্ন উত্তর" &&
+                dropdownVisible === "সাধারণ প্রশ্ন উত্তর" && (
+                  <div
+                    onMouseEnter={() =>
+                      setDropdownVisible("সাধারণ প্রশ্ন উত্তর")
+                    }
+                    onMouseLeave={() => setDropdownVisible(null)}
+                    className="text-md w-48 absolute top-full left-0 mt-2 bg-white shadow-lg z-10"
+                  >
+                    {generalQOptions.map((option, idx) => (
+                      <a
+                        key={idx}
+                        href={option.link || "#"} // Only add href if a link exists
+                        className={`block px-4 py-2 text-black hover:border-b-2 border-white hover:text-white ${
+                          option.link ? "cursor-pointer" : "cursor-default"
+                        }`}
+                      >
+                        {option.label}
+                      </a>
+                    ))}
+                  </div>
+                )}
 
               {index < tabs.length - 1 && (
                 <div className="h-6 w-px bg-gray-300 opacity-50 mx-2" /> // Vertical Divider
