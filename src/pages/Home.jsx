659bdc0cee26f243bbea4f876decf102aa2847e9
Showrab <supto50showrab@gmail.com>
2025-11-07 20:52:22 +0600

    bye bye bro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Products from "../components/Products/Products";
 export default function Home() {
   return (
     <>
+    <p className="text-red-500 text center my-10 text2xl">Error 404</p>
       {/* <Banner />
       <Products /> */}
     </>
